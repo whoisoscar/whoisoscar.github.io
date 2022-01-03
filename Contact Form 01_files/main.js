a8f1eb697f375a8d62c4050d29904632c2e7400e
Oscar <39652888+whoisoscar@users.noreply.github.com>
2022-01-03 20:05:11 +0100

    Update main.js

--- Contact Form 01_files/main.js
+++ Contact Form 01_files/main.js
@@ -1,7 +1,7 @@
 function calc(){
     document.getElementById("entry").innerText = document.getElementById("entryPrice").value
-    document.getElementById("takeProfit").innerText = (document.getElementById("entryPrice").value * (1+document.getElementById("takeProfitP").value / 100)).toFixed(3)
-    document.getElementById("stopLoss").innerText = (document.getElementById("entryPrice").value * (1-document.getElementById("stopLossP").value / 100)).toFixed(3)
+    document.getElementById("takeProfit").innerText = (document.getElementById("entryPrice").value * (1+document.getElementById("takeProfitP").value / 100)).toFixed(4)
+    document.getElementById("stopLoss").innerText = (document.getElementById("entryPrice").value * (1-document.getElementById("stopLossP").value / 100)).toFixed(4)
     document.getElementById("risk").innerText = document.getElementById("posSize").value * (1-document.getElementById("stopLossP").value / 100) - document.getElementById("posSize").value
     document.getElementById("reward").innerText = document.getElementById("posSize").value * (1+document.getElementById("takeProfitP").value / 100) - document.getElementById("posSize").value
 
