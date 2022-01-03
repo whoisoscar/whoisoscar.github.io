b1b29ee20ec53362600c9a9368840d01e8394244
Oscar <39652888+whoisoscar@users.noreply.github.com>
2022-01-03 15:36:12 +0100

    Add files via upload

---
+++ Contact Form 01_files/main.js
@@ -0,0 +1,12 @@
+function calc(){
+    document.getElementById("entry").innerText = document.getElementById("entryPrice").value
+    document.getElementById("takeProfit").innerText = document.getElementById("entryPrice").value * (1+document.getElementById("takeProfitP").value / 100)
+    document.getElementById("stopLoss").innerText = document.getElementById("entryPrice").value * (1-document.getElementById("stopLossP").value / 100)
+    document.getElementById("risk").innerText = document.getElementById("posSize").value * (1-document.getElementById("stopLossP").value / 100) - document.getElementById("posSize").value
+    document.getElementById("reward").innerText = document.getElementById("posSize").value * (1+document.getElementById("takeProfitP").value / 100) - document.getElementById("posSize").value
+
+}
+
+function copyValue(id){
+    navigator.clipboard.writeText(document.getElementById(id).innerText)
+}
