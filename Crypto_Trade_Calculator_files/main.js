b52892e070030d3968dea78a0bf7973a46574e85
Oscar <39652888+whoisoscar@users.noreply.github.com>
2022-01-10 17:03:49 +0100

    Update main.js

--- Crypto_Trade_Calculator_files/main.js
+++ Crypto_Trade_Calculator_files/main.js
@@ -2,8 +2,8 @@ function calc(){
     document.getElementById("entry").innerText = document.getElementById("entryPrice").value
     document.getElementById("takeProfit").innerText = (document.getElementById("entryPrice").value * (1+document.getElementById("takeProfitP").value / 100)).toFixed(4)
     document.getElementById("stopLoss").innerText = (document.getElementById("entryPrice").value * (1-document.getElementById("stopLossP").value / 100)).toFixed(4)
-    document.getElementById("risk").innerText = document.getElementById("posSize").value * (1-document.getElementById("stopLossP").value / 100) - document.getElementById("posSize").value
-    document.getElementById("reward").innerText = document.getElementById("posSize").value * (1+document.getElementById("takeProfitP").value / 100) - document.getElementById("posSize").value
+    document.getElementById("risk").innerText = (document.getElementById("posSize").value * (1-document.getElementById("stopLossP").value / 100) - document.getElementById("posSize").value).toFixed(4)
+    document.getElementById("reward").innerText = (document.getElementById("posSize").value * (1+document.getElementById("takeProfitP").value / 100) - document.getElementById("posSize").value).toFixed(4)
     document.getElementById("tokenAmount").innerText = (document.getElementById("posSize").value / document.getElementById("entryPrice").value).toFixed(4)
 
 }
